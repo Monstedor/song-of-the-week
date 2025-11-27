cd1d251f520a2123528c824e1864ce24534387a6
Monstedor <github@tenteren.pl>
2025-11-27 18:02:24 +0100

    Update server.js
    dodalem pusty komentarz

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.set('trust proxy', 1); 
+app.set('trust proxy', 1);
+// pusty komentarz
 // Middleware
 app.use(cors());
 app.use(express.json());
