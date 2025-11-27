b0ea7f45eaa9785b0b9bd009b996f639482008fc
Monstedor <github@tenteren.pl>
2025-11-27 17:29:59 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1); 
 // Middleware
 app.use(cors());
 app.use(express.json());
